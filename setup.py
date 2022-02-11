a65e52c5c148f916b8953977fbd5e44127b7b93b
mateczentye <czentyemate@gmail.com>
2022-02-11 13:14:20 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Magnon Dispersion Plotter',
+    author='Mate Czentye',
+    version='0.0.1',
+    author_email='czentyemate@gmail.com',
+    license='MIT',
+    url='https://github.com/mateczentye/',
+    description='Package plots 2D, and 3D the Magnon dispersion in k-space',
+    packages=["magnons"],
+)
